232a22c082c4f7bf872b6f275a13541bbb5dca71
aaron.pillert <aaron.pillert@ark.org>
2020-05-01 16:58:09 -0500

    Initial commit

---
+++ database/migrations/2020_05_01_214310_create_worlds_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateWorldsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('worlds', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name');
+            $table->enum('status', ['active', 'inactive']);
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('worlds');
+    }
+}
